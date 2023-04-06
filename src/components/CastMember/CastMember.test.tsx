280d36bb6ff5329e51032238fecce3d78825a32e
Aloys Berger <aloys.berger@technichegroup.com>
2023-04-06 14:39:15 +1000

    feat(CastMember): Extract the CastMember component

---
+++ src/components/CastMember/CastMember.test.tsx
@@ -0,0 +1,29 @@
+import { CastMember, Props } from "./CastMember";
+import { render, screen } from "@testing-library/react";
+describe("components/CastMember", () => {
+   const defaultProps: Props = {
+      member: {
+         person: {
+            name: "Test Person",
+            image: {
+               medium: "test-medium.jpg",
+            },
+         },
+         character: {
+            name: "Test Character",
+         },
+      },
+   };
+   it("renders the cast member details", () => {
+      const castMember = defaultProps.member;
+      render(<CastMember {...defaultProps} />);
+      expect(screen.getByRole("img")).toHaveAttribute(
+         "src",
+         castMember.person.image.medium
+      );
+      expect(screen.getByText(castMember.person.name)).toBeInTheDocument();
+      expect(screen.getByTestId("cast-name")).toHaveTextContent(
+         castMember.character.name
+      );
+   });
+});
