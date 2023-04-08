6450422dd2396cd9e0691553bbb27858e31cb33b
Aloys Berger <alo@reaktive.com>
2023-04-08 16:31:18 +1000

    style(results): Styling improvements on the results page

--- src/components/LandingBackground/Poster.tsx
+++ src/components/LandingBackground/Poster.tsx
@@ -4,7 +4,7 @@ export function Poster(): JSX.Element {
    const url = getRandomShowImageUrl()
    return (
       <div className={`w-40 rounded-md shadow-sm bg-slate-950 h-52 relative shadow-slate-900/10`}>
-         <div className="absolute z-10 w-full h-full bg-slate-950/50"></div>
+         <div className="absolute w-full h-full bg-slate-950/50"></div>
          <img src={url} alt="poster" className="object-cover w-full h-full rounded-md" />
       </div>
    );
