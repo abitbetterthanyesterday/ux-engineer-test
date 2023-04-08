d8d3bf764ca6439e694ecb74de13d3a0add255af
Aloys Berger <alo@reaktive.com>
2023-04-09 09:55:45 +1000

    fix(result-screen): Remove the animation for the detail result page, style the detail result page

--- src/components/CastMember/CastMember.tsx
+++ src/components/CastMember/CastMember.tsx
@@ -6,15 +6,15 @@ export interface Props {
 
 export function CastMember({ member }: Props): JSX.Element {
    return (
-      <div className="cast-member">
-         <div className="cast-member-image">
+      <div>
+         <div className="w-24 overflow-hidden rounded-lg shadow md:w-48">
             {member.person.image && (
                <img src={member.person.image.medium} alt="" />
             )}
          </div>
-         <p data-testid="cast-name">
-            <strong>{member.person.name}</strong>
-            as {member.character.name}
+         <p data-testid="cast-name" className="flex flex-col items-center justify-center py-2">
+            <strong className="text-center">{member.person.name}</strong>
+            <em className="text-center">{member.character.name}</em>
          </p>
       </div>
    );
--- src/components/ShowList/ShowList.tsx
+++ src/components/ShowList/ShowList.tsx
@@ -15,14 +15,7 @@ export function ShowList({ shows, onSelectShow }: Props): JSX.Element {
          <ul className={`flex items-start flex-grow gap-8 px-24 pt-4 -mx-12 overflow-x-scroll cursor-pointer md:grid-cols-3 md:grid scroll-smooth ${shows.length === 1 && 'justify-center'}`}>
             {shows.map((show) => <ShowListCard key={show.id} show={show} />)}
          </ul>
-         {/* {
-            translate &&
-            <div className="absolute top-0 left-0 w-screen h-screen bg-gradient-to-t from-slate-950 via-slate-950 to-slate-950/50 via-65% flex flex-col p-4 animate-fadeIn duration-1000 transition" onClick={() => setTranslate(false)}>
-               <div className="h-1/2"></div>
-               <h1>All the information</h1>
-               <h2>{translate}</h2>
-            </div>
-         } */}
+
       </div >
    );
 }
