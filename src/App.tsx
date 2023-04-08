85b7e3d627515f072c217637694c0f7fdca978ce
Aloys Berger <alo@reaktive.com>
2023-04-08 15:02:14 +1000

    style(landing-bg): Improve the landing background placement of posters

--- src/App.tsx
+++ src/App.tsx
@@ -15,48 +15,49 @@ export default function App(): JSX.Element {
    } = useSearchShow();
 
 
-   if(!hasSearched){
+   if (!hasSearched) {
 
-   return (
-      <main className="relative flex flex-col items-center justify-center w-screen min-h-screen px-8 overflow-hidden font-sans transition-all">
-
-   <div className="flex flex-col items-center w-screen h-screen overflow-x-hidden">
-         <LandingBackground/>
-         <div className="flex flex-col items-center justify-center w-screen h-screen gap-4 p-16 mb-12">
-            <Logo/>
-            <SearchInput/>
-         </div>
-</div>
-         {error && <div>{error}</div>}
-
-      </main>
-   )}
-
-
-
-
-return(<div className={`w-screen top-full h-[100vh]  text-slate-50 shadow-lg duration-500`}>
-   <div className={`bg-gradient-to-t from-slate-950 to-indigo-950 w-screen top-full h-screen z-20 transition-transform duration-1000 text-slate-50 shadow-xl border-t border-indigo-800 flex flex-col py-12`}>
-      <SearchInput/>
-               <div className="flex-col items-start px-12">
-      <button
-   onClick={backToSearch}
-                  className="px-4 py-2 text-sm font-bold tracking-wide uppercase transition bg-indigo-800 border-t-2 border-b rounded-md shadow-lg text-slate-50 shadow-indigo-950/10 animation-pulse border-b-indigo-950 border-t-indigo-50/10 active:border-t-0 active:bg-indigo-800/50 active:border-b active:border-b-indigo-700 active:text-slate-50/80 group active:shadow-inner hover:bg-indigo-700 hover:scale-[1.02] focus:outline-none focus:ring-4 ring-indigo-50 flex-grow-0"
-   
-   >Back to Search</button>
-                     <div className="pt-4 -mb-2">
-                     {shows.length === 0 ? (<h1>Oops no results</h1>):(
-                        <>
-                        <span className="py-4 text-sm font-semibold tracking-wide uppercase text-slate-50/80">
-                           {shows.length} results for "{query}"
-                        </span>
-                        </>
-                     )}
-                     </div>
-                  
-                  <ShowList shows={shows} onSelectShow={onSelectShow} />
+      return (
+         <>
+            <LandingBackground />
+            <main className="relative flex flex-col items-center justify-center w-screen min-h-screen px-8 overflow-hidden font-sans transition-all">
+               <div className="flex flex-col items-center w-screen h-screen overflow-x-hidden">
+                  <div className="flex flex-col items-center justify-center w-screen h-screen gap-4 p-16 mb-12">
+                     <Logo />
+                     <SearchInput />
+                  </div>
                </div>
+               {error && <div>{error}</div>}
+            </main>
+         </>
+      )
+   }
+
+
+
+
+   return (<div className={`w-screen top-full h-[100vh]  text-slate-50 shadow-lg duration-500`}>
+      <div className={`bg-gradient-to-t from-slate-950 to-indigo-950 w-screen top-full h-screen z-20 transition-transform duration-1000 text-slate-50 shadow-xl border-t border-indigo-800 flex flex-col py-12`}>
+         <SearchInput />
+         <div className="flex-col items-start px-12">
+            <button
+               onClick={backToSearch}
+               className="px-4 py-2 text-sm font-bold tracking-wide uppercase transition bg-indigo-800 border-t-2 border-b rounded-md shadow-lg text-slate-50 shadow-indigo-950/10 animation-pulse border-b-indigo-950 border-t-indigo-50/10 active:border-t-0 active:bg-indigo-800/50 active:border-b active:border-b-indigo-700 active:text-slate-50/80 group active:shadow-inner hover:bg-indigo-700 hover:scale-[1.02] focus:outline-none focus:ring-4 ring-indigo-50 flex-grow-0"
+
+            >Back to Search</button>
+            <div className="pt-4 -mb-2">
+               {shows.length === 0 ? (<h1>Oops no results</h1>) : (
+                  <>
+                     <span className="py-4 text-sm font-semibold tracking-wide uppercase text-slate-50/80">
+                        {shows.length} results for "{query}"
+                     </span>
+                  </>
+               )}
+            </div>
+
+            <ShowList shows={shows} onSelectShow={onSelectShow} />
          </div>
-</div>
-)
+      </div>
+   </div>
+   )
 }
--- src/components/LandingBackground/LandingBackground.tsx
+++ src/components/LandingBackground/LandingBackground.tsx
@@ -10,10 +10,12 @@ function LandingBackground(): JSX.Element {
     }
 
          return (
-         <div className="absolute flex w-screen h-full min-w-full gap-6 overflow-hidden bg-indigo-950 bg-gradient-to-t from-gray-900 via-slate-950 to-indigo-950 -z-10">
+          <div className="absolute w-screen h-screen overflow-hidden -z-10">
+         <div className="absolute flex w-[120vh] h-full min-w-full gap-6 overflow-hidden bg-indigo-950 bg-gradient-to-t from-gray-900 via-slate-950 to-indigo-950 -z-10 -ml-[2vh]">
             <div className="absolute top-0 left-0 z-20 w-full h-full transform bg-gradient-radial from-slate-950/80 via-slate-950/40 to-indigo-500/20 via-35%"/>
             {Array.from({length:numbersOfColumns}, (_, i) => <PostersColumn key={i} index={i}/> )}
          </div>
+</div>
          )
 }
 
