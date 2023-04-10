722a5a229b965cc942a29692e1c3a0bc793f0282
Aloys Berger <alo@reaktive.com>
2023-04-10 16:05:45 +1000

    fix(landing-page): Review the color of the background, add animation to landing screen fade in

--- src/components/LandingBackground/LandingBackground.tsx
+++ src/components/LandingBackground/LandingBackground.tsx
@@ -20,7 +20,7 @@ function LandingBackground(): JSX.Element {
   }
 
   return (
-    <div className="absolute z-10 w-screen h-screen overflow-hidden">
+    <div className="absolute z-10 w-screen h-screen overflow-hidden animate-fadeIn">
       <div className="absolute flex w-[120vh] h-full min-w-full gap-6 overflow-hidden bg-indigo-950 bg-gradient-to-t from-gray-900 via-slate-950 to-indigo-950 -z-10 -ml-[2vh]">
         <div className="absolute top-0 left-0 z-20 w-full h-full transform bg-gradient-radial from-slate-950/80 via-slate-950/50 to-indigo-950/20 via-45%" />
         {Array.from({ length: columnsCount }, (_, i) => <PostersColumn key={i} index={i} />)}
